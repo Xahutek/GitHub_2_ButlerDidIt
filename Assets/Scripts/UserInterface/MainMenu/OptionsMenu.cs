7ac4a27a959dca84a206ab775cd968bd02644761
Lupimerliph <107180614+Lupimerliph@users.noreply.github.com>
2022-06-15 15:43:08 +0200

    Main Menu Options Functions
    Not sure if the volume slider works how it should. Fullscreen and resolution worked in build

--- Assets/Scripts/UserInterface/MainMenu/OptionsMenu.cs
+++ Assets/Scripts/UserInterface/MainMenu/OptionsMenu.cs
@@ -1,23 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
+    #region Player Pref Key Constants
+    private const string RESOLUTION_PRED_KEY = "resolution";
+    #endregion
     public GameObject fullScreenCross;
+    List<Resolution> resos = new List<Resolution>();
+
+    public TMP_Text resTxt;
+    private int ResIndex = 0;
+    [SerializeField] private Slider volumeSlider = null;
+
+    private void Start()
+    {
+        foreach (Resolution resolution in Screen.resolutions)
+            if (resolution.width / resolution.height == 16 / 9)
+                resos.Add(resolution);
+        ResIndex = PlayerPrefs.GetInt(RESOLUTION_PRED_KEY, 0);
+    }
 
     public void ToggleFullScreen()
     {
-        if (Screen.fullScreenMode == FullScreenMode.ExclusiveFullScreen)
+        if (Screen.fullScreenMode == FullScreenMode.Windowed)
         {
-            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
             fullScreenCross.SetActive(true);
         }
         else
         {
             Screen.fullScreenMode = FullScreenMode.Windowed;
-            fullScreenCross.SetActive(true);
+            fullScreenCross.SetActive(false);
         }
+    }
 
+    public void SetResButton(bool increment)
+    {
+        ResIndex = increment && ResIndex >= resos.Count - 1 ? 0 : 
+            !increment && ResIndex <= 0 ? resos.Count - 1 : increment ? ResIndex +1 : ResIndex-1;
+        Resolution res = resos[ResIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreenMode);
+        SetResText(res);
+    }
+
+    private void SetResText(Resolution res)
+    {
+        resTxt.text = res.width + " x " + res.height;
+    }
+
+    public void VolumeSlider(float volume)
+    {
+        PlayerPrefs.SetFloat("Master_Volume", Mathf.Round(volume * 100.0f) * 0.01f);
     }
 }
