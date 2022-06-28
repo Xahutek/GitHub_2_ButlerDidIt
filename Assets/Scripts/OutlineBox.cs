2b38483f9676402f1b2794c37f0c0438f3ea7e76
Oliver-boop-blip <83859423+Oliver-boop-blip@users.noreply.github.com>
2022-06-28 20:31:33 +0200

    Hab mal die events aus gemacht weil mir das so derbe auf die Klöten geht

--- Assets/Scripts/OutlineBox.cs
+++ Assets/Scripts/OutlineBox.cs
@@ -7,8 +7,12 @@ public class OutlineBox : MonoBehaviour
     [SerializeField] Material outline;
     // Start is called before the first frame update
     [SerializeField] BoxCollider2D boxColliderTwoDee;
+    Transform player;
+    [SerializeField] float ItemRadius = 1;
+    private float distance;
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         boxColliderTwoDee = GetComponent<BoxCollider2D>();
         outline.SetColor("Color_cb38644a3f444f6cb498ab0e82528ebb", Color.black);
         outline.SetFloat("Vector1_e2aa71b3209842c5a6eb0b87444d3361", 0);
@@ -17,7 +21,19 @@ public class OutlineBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
+        distance = Vector2.Distance(this.transform.position, player.transform.position);
+        Debug.Log(this.name+distance);
+        if (ItemRadius>distance)
+        {
+            outline.SetColor("Color_cb38644a3f444f6cb498ab0e82528ebb", Color.white);
+            outline.SetFloat("Vector1_e2aa71b3209842c5a6eb0b87444d3361", 0.1f);
+        }
+        if (ItemRadius < distance)
+        {
+            outline.SetColor("Color_cb38644a3f444f6cb498ab0e82528ebb", Color.black);
+            outline.SetFloat("Vector1_e2aa71b3209842c5a6eb0b87444d3361", 0);
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
