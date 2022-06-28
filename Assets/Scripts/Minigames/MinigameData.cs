81ed71e1710d16fa9bbbf1ab017f5338f846fe65
Xahutek <xahutek11@gmail.com>
2022-06-28 14:58:13 +0200

    new chess minigame!

--- Assets/Scripts/Minigames/MinigameData.cs
+++ Assets/Scripts/Minigames/MinigameData.cs
@@ -8,52 +8,18 @@ public static class MinigameData
     //Calendar
     public static List<bool> calendar;
 
-    //Chess
-    [System.Serializable]
-    public class ChessFigure
-    {
-        public Vector2Int coordinates;
-
-        public ChessFigure(Vector2 _coords)
-        {
-            coordinates = new Vector2Int(Mathf.FloorToInt(_coords.x), Mathf.FloorToInt(_coords.y));
-        }
-        public ChessFigure(ProgressFile.Minigames_ProgressFile.SerializedVector2 _coords)
-        {
-            coordinates = new Vector2Int(Mathf.FloorToInt(_coords.x), Mathf.FloorToInt(_coords.y));
-        }
-        public ChessFigure(ChessBoard.ChessFigure figure)
-        {
-            coordinates = figure.slot.Coordinates;
-        }
-    }
-    public static ChessFigure
-        blackKing,
-        whiteKing;
-    public static ChessFigure[]
-        blackfigures = new ChessFigure[0],
-        whiteFigures = new ChessFigure[0];
+    ////Chess
+    public static List<Vector2Int> slotFigures = new List<Vector2Int>();
 
     public static void SaveTo(ChessBoard.GameManager manager)
     {
-        List<ChessFigure>
-            listBlack = new List<ChessFigure>(),
-            listWhite = new List<ChessFigure>();
+        slotFigures = new List<Vector2Int>();
 
-        blackKing = new ChessFigure(manager.BlackKing);
-        whiteKing = new ChessFigure(manager.WhiteKing);
-
-        foreach (ChessBoard.ChessFigure figure in manager.blackFigures)
+        for (int i = 0; i < manager.slots.Count; i++)
         {
-            listBlack.Add(new ChessFigure(figure));
+            ChessBoard.BoardSlot slot = manager.slots[i];
+            slotFigures.Add(new Vector2Int(i,slot.figure==null?-1:manager.figures.IndexOf(slot.figure)));
         }
-        foreach (ChessBoard.ChessFigure figure in manager.whiteFigures)
-        {
-            listWhite.Add(new ChessFigure(figure));
-        }
-
-        blackfigures = listBlack.ToArray();
-        whiteFigures = listWhite.ToArray();
     }
 
     public static void SaveTo(Calendar.GameManager manager)
