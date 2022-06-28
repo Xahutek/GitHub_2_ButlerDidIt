fc4ff979c753b671992e0240e1ab8d200496d4df
Oliver-boop-blip <83859423+Oliver-boop-blip@users.noreply.github.com>
2022-06-28 15:08:13 +0200

    burn effect

--- Assets/Scripts/Minigames/InvisibleLetters/PaperSpot.cs
+++ Assets/Scripts/Minigames/InvisibleLetters/PaperSpot.cs
@@ -11,10 +11,12 @@ namespace Letters
         public Color inkColor;
         private Coroutine heating;
         [HideInInspector] public float time, minHoldTime = 1.5f, completeTime = 4;
+        [SerializeField] Material dissolve;
 
         private void Awake()
         {
             text = GetComponentInChildren<TMP_Text>();
+            dissolve.SetFloat("Vector1_6298484c3b4e4b1fbafd65018b3aba39", 0);
         }
 
         public void InkStatus()
@@ -43,8 +45,19 @@ namespace Letters
             {
                 text.color = Color.Lerp(Color.clear, inkColor, EaseIn(time/(completeTime+ minHoldTime)));
                 yield return null;
+
             }
             GameManager.main.SetPaperState();
+            while (GameManager.main.gameState == GameState.Deciphered && time >= (completeTime + minHoldTime))
+            {
+                //Time from 5.5 to 10.9
+                //b1 + (s1-a1)*(b2-b1)/(a2-a1)
+                // time, 5.5, 10.9, 0,2
+                Debug.Log(0+(time-5.5f)*(2-0)/(10.9f-5.5f));
+                dissolve.SetFloat("Vector1_6298484c3b4e4b1fbafd65018b3aba39", 0+(time-5.5f)*(2-0)/(12f-5.5f));
+                text.color = Color.Lerp(inkColor, Color.clear, EaseIn(time / (completeTime + minHoldTime)));
+                yield return null;
+            }
         }
 
         public static float EaseIn(float t) { return  Mathf.Pow(t,3);}
