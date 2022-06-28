cdc233f87d5a763e628a4ad501f9f9ed074b8141
Oliver-boop-blip <83859423+Oliver-boop-blip@users.noreply.github.com>
2022-06-28 18:33:25 +0200

    Neue CrossExam

--- Assets/Scripts/OutlineBox.cs
+++ Assets/Scripts/OutlineBox.cs
@@ -6,8 +6,10 @@ public class OutlineBox : MonoBehaviour
 {
     [SerializeField] Material outline;
     // Start is called before the first frame update
+    [SerializeField] BoxCollider2D boxColliderTwoDee;
     void Start()
     {
+        boxColliderTwoDee = GetComponent<BoxCollider2D>();
         outline.SetColor("Color_cb38644a3f444f6cb498ab0e82528ebb", Color.black);
         outline.SetFloat("Vector1_e2aa71b3209842c5a6eb0b87444d3361", 0);
     }
@@ -15,17 +17,25 @@ public class OutlineBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Outline on");
         outline.SetColor("Color_cb38644a3f444f6cb498ab0e82528ebb", Color.white);
-        outline.SetFloat("Vector1_e2aa71b3209842c5a6eb0b87444d3361", 0.5f);
+        outline.SetFloat("Vector1_e2aa71b3209842c5a6eb0b87444d3361", 0.1f);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
+        Debug.Log("Outline off");
         outline.SetColor("Color_cb38644a3f444f6cb498ab0e82528ebb", Color.black);
         outline.SetFloat("Vector1_e2aa71b3209842c5a6eb0b87444d3361", 0);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Outline on");
+        outline.SetColor("Color_cb38644a3f444f6cb498ab0e82528ebb", Color.white);
+        outline.SetFloat("Vector1_e2aa71b3209842c5a6eb0b87444d3361", 0.1f);
     }
 
 }
