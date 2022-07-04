06907c518d12f003394f83d38dd269526d738306
Xahutek <xahutek11@gmail.com>
2022-07-04 15:21:46 +0200

    Clue Giver now takes conditions

--- Assets/Scripts/Suspicion/ClueGiver.cs
+++ Assets/Scripts/Suspicion/ClueGiver.cs
@@ -8,10 +8,24 @@ public class ClueGiver : MonoBehaviour
     public Character Character = Character.Detective;
     public float time;
 
+    public Clue[] ButlerKnowsCondition, ButlerNotKnowsCondition;
+
     private void FixedUpdate()
     {
         if (time >= Clock.Hour && !Yield.KnownTo(Character))
         {
+            foreach (Clue c in ButlerKnowsCondition)
+            {
+                if (!c.KnownTo(Character.Butler))
+                    return;
+            }
+
+            foreach (Clue c in ButlerNotKnowsCondition)
+            {
+                if (c.KnownTo(Character.Butler))
+                    return;
+            }
+
             Yield.MakeKnownTo(Character);
             Debug.Log(Character+" found out about "+ Yield.name +" because it is past "+time);
         }
