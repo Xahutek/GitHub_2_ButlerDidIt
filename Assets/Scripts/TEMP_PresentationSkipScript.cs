6bb7e1c0430b5e4e04efb19aedb39a8fea66d1f6
Xahutek <xahutek11@gmail.com>
2022-07-07 10:03:32 +0200

    Final

--- Assets/Scripts/TEMP_PresentationSkipScript.cs
+++ Assets/Scripts/TEMP_PresentationSkipScript.cs
@@ -4,5 +4,46 @@ using UnityEngine;
 
 public class TEMP_PresentationSkipScript : MonoBehaviour
 {
-    
+    public float[] times;
+
+    public CharacterProfile[] KnownCharacters;
+    public Clue[] KnownClues, KnownCluesDetective;
+
+    public Clue confessionClue;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.N)) SkipToNext();
+        if (Input.GetKeyUp(KeyCode.M)) Done();
+    }
+
+    public void SkipToNext()
+    {
+        foreach (Clue c in KnownCluesDetective)
+        {
+            c.MakeKnownTo(Character.Detective);
+        }
+        foreach (Clue c in KnownClues)
+        {
+            c.MakeKnownTo(Character.Butler);
+        }
+        foreach (CharacterProfile c in KnownCharacters)
+        {
+            c.knownToPlayer = true;
+        }
+
+        foreach (float time in times)
+        {
+            if (Clock.Hour < time)
+            {
+                Clock.SetTime(time);
+                EventSystem.main.RefreshRooms();
+                return;
+            }
+        }
+    }
+    public void Done()
+    {
+        confessionClue.MakeKnownTo(Character.Detective);
+    }
 }
