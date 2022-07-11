809437f157cca0b2f4ccd25470cd74472c4b15e9
Lupimerliph <107180614+Lupimerliph@users.noreply.github.com>
2022-07-11 13:01:45 +0200

    Fix early Eva

--- Assets/Scripts/Characters/ImposterSwitch.cs
+++ Assets/Scripts/Characters/ImposterSwitch.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ImposterSwitch : MonoBehaviour
 {   
-    void Start()
+    void OnEnable()
     {
         InteractableCharacter cha = GetComponentInParent<InteractableCharacter>();
         if (cha != null)
